6c0faf76ab1e9bb4b704cf9f2012c86f17aa3ed7
Niwas <niwas6996@gmail.com>
2025-10-27 22:24:19 +0530

    dependencies

---
+++ models/demo/employee.sql
@@ -0,0 +1,14 @@
+-- models/employee/employee.sql
+
+{{ config(
+    materialized='table'
+) }}
+
+SELECT
+    emp_id,
+    first_name,
+    last_name,
+    department,
+    salary,
+    hire_date
+FROM {{ source('fivetran', 'public') }};
