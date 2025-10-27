6fc9c3b0a358615da5d330167c7f445a3d844187
Niwas <niwas6996@gmail.com>
2025-10-27 22:29:45 +0530

    dependencies

--- models/demo/employee.sql
+++ models/demo/employee.sql
@@ -1,14 +1,9 @@
--- models/employee/employee.sql
-
-{{ config(
-    materialized='table'
-) }}
+{{ config(materialized='table') }}
 
 SELECT
     emp_id,
     first_name,
     last_name,
     department,
-    salary,
     hire_date
-FROM {{ source('fivetran', 'employee') }};
+FROM {{ source('fivetran', 'employees') }}
